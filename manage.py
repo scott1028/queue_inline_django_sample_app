64fdd79c8546384e9df5dd1942f94b939bb4d92b
Scott <mic1028002@gmail.com>
2015-01-16 00:19:39 +0800

    update code

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
 import os
 import sys
 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "queue_inline.settings")
-
-    # sys.path.append(os.path.join(os.path.dirname(__file__), "venv\\Lib\\site-packages\\"))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "venv\\Lib\\site-packages"))
 
     from django.core.management import execute_from_command_line
 
